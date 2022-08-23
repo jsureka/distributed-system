8c35803e13c00864f21ecb862180e4371a3275d0
jsureka <bsse1115@iit.du.ac.bd>
2022-08-23 13:37:14 +0600

    routes changes

--- statusService/routes/statusRoute.js
+++ statusService/routes/statusRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
  createStatus, getStatus
 } = require("../controllers/statusController");
-const { isAuthenticatedUser } = require("../middleware/auth");
+//const { isAuthenticatedUser } = require("../middleware/auth");
 const multer = require('multer');
 var upload = multer()
 
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.route("/status").post(upload.array(), createStatus);
 
-router.route("/status").get(isAuthenticatedUser, getStatus);
+router.route("/status").get(getStatus);
 
 
 module.exports = router;
